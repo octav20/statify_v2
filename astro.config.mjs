927f849e12749a8d665d19bc3cd386a48806a4c4
Octavio Guerrero <97319614+octav20@users.noreply.github.com>
2025-06-04 16:18:59 -0700

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -15,9 +17,7 @@ export default defineConfig({
 
   output: 'server',
 
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
 
   integrations: [react()]
 });
